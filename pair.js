44ab822e1d407d2006da9c5feabd4031877f703d
chamodxxc <nimsarawanas.inhe@gmail.com>
2025-09-13 23:10:27 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -24,7 +24,7 @@ const {
     prepareWAMessageMedia,
     generateWAMessageFromContent,
     S_WHATSAPP_NET
-} = require('@whiskeysockets/baileys');
+} = require('baileys');
 
 const config = {
     AUTO_VIEW_STATUS: 'true',
